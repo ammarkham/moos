c8637ececd24f2c330b1ea58590bee8ebcc628d1
Andres Molina-Markham <amolina@cs.umass.edu>
2011-09-30 00:30:20 -0400

    Adding a function to set the clock in the hardware initialization

--- Demo/MSP430X_Moo_IAR/main.c
+++ Demo/MSP430X_Moo_IAR/main.c
@@ -32,6 +32,29 @@
 portBASE_TYPE xLocalError = pdFALSE;
 volatile unsigned portLONG ulIdleLoops = 0UL;
 
+///BEGIN DCO Clock
+/* From: msp430x261x_fll_01.c (slac151d)
+Set DCO clock to (Delta)*(4096) using software FLL. DCO clock
+is output on P1.4 as SMCLK.  DCO clock, which is the selected SMCLK source
+for timer_A is integrated over LFXT1/8 (4096) until SMCLK is equal to Delta
+CCR2 captures ACLK.  To use Set_DCO Timer_A must be operating in continous
+mode.  Watch crystal for ACLK is required for this example.  Delta must be
+kept in a range that allows possible DCO speeds.  Minimum Delta must ensure
+that Set_DCO loop can complete within capture interval. Maximum delta can be 
+calculated by f(DCOx7) / 4096.  f(DCOx7) can be found in device specific
+datasheet. ACLK = LFXT1/8 = 32768/8, MCLK = SMCLK = target DCO
+External watch crystal installed on XIN XOUT is required for ACLK */
+//formula is DELTA*ACLK = frequency you want!!
+#define DELTA 2930                      // target DCO = DELTA*(4096) = 12MHz
+//#define DELTA 2445                        // target DCO = DELTA*(4096) = 10MHz
+//#define DELTA 900                       // target DCO = DELTA*(4096) = 3686400
+//#define DELTA 256                       // target DCO = DELTA*(4096) = 1048576
+//#define DELTA 70                        // target DCO = DELTA*(4096) = 286720
+
+volatile unsigned int i;
+void Set_DCO(void);
+///END DCO Clock
+
 // Uncomment the following to test the LEDs
 
 /*
@@ -67,8 +90,6 @@ void main( void )
             // Toggle the first four LEDs (on the assumption there are at least 4 fitted. 
             vParTestToggleLED( 0 );
             vParTestToggleLED( 1 );
-            //vParTestToggleLED( 2 );
-            //vParTestToggleLED( 3 );
         }
 
         //return 0;
@@ -81,6 +102,11 @@ static void prvSetupHardware( void )
 	/* Stop the watchdog. */
 	WDTCTL = WDTPW + WDTHOLD;
          
+        
+        for (i = 0; i < 0xfffe; i++);             // Delay for XTAL stabilization
+        P1DIR |= 0x10;                            // P1.4 output
+        P1SEL |= 0x10;                            // P1.4 SMCLK output
+        Set_DCO();   
 
 	/* Setup DCO+ for ( xtal * D * (N + 1) ) operation. */
 	//FLL_CTL0 |= DCOPLUS + XCAP18PF;
@@ -99,6 +125,45 @@ static void prvSetupHardware( void )
 	P4SEL = 0xFC;
 	P5SEL = 0xFF;
 }
+
+void Set_DCO(void)                          // Set DCO to selected frequency
+{
+  unsigned int Compare, Oldcapture = 0;
+
+  BCSCTL1 |= DIVA_3;                        // ACLK= LFXT1CLK/8 = 4096Hz
+  TACCTL2 = CM_1 + CCIS_1 + CAP;            // CAP, ACLK
+  TACTL = TASSEL_2 + MC_2 + TACLR;          // SMCLK, cont-mode, clear
+
+  while (1)
+  {
+    while (!(CCIFG & TACCTL2));             // Wait until capture occured
+    TACCTL2 &= ~CCIFG;                      // Capture occured, clear flag
+    Compare = TACCR2;                       // Get current captured SMCLK
+    Compare = Compare - Oldcapture;         // SMCLK difference
+    Oldcapture = TACCR2;                    // Save current captured SMCLK
+
+    if (DELTA == Compare)
+      break;                                // If equal, leave "while(1)"
+    else if (DELTA < Compare)
+    {
+      DCOCTL--;                             // DCO is too fast, slow it down
+      if (DCOCTL == 0xFF)                   // Did DCO roll under?
+        if (BCSCTL1 & 0x0f)
+          BCSCTL1--;                        // Select lower RSEL
+    }
+    else
+    {
+      DCOCTL++;                             // DCO is too slow, speed it up
+      if (DCOCTL == 0x00)                   // Did DCO roll over?
+        if ((BCSCTL1 & 0x0f) != 0x0f)
+          BCSCTL1++;                        // Sel higher RSEL
+    }
+  }
+  TACCTL2 = 0;                              // Stop TACCR2
+  TACTL = 0;                                // Stop Timer_A
+  BCSCTL1 &= ~DIVA_3;                       // ACLK = LFXT1CLK = 32.768KHz
+}
+
 
 int main( void )
     {
